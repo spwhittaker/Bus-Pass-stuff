8379550d876a2a5791b10706d16ced20b3ddde5c
Stephen Whittaker <stephenwhittaker23@gmail.com>
2019-09-27 07:28:21 +0100

    fixed for £ sign entries

--- bus-pass.js
+++ bus-pass.js
@@ -3,16 +3,29 @@ var busPassCost = prompt("How much is your bus pass?");
 var weeksForPass = prompt("How many weeks is it valid for?");
 var daysPerWeek = prompt("How many days on average will you use the bus pass per week?");
 var daySaver =  prompt("How much is a daily bus ticket?");
-var daySaverNum = parseFloat(daySaver);
-var busPassCostNum = parseFloat(busPassCost);
-var dailyPassCost = (busPassCostNum / (weeksForPass * daysPerWeek));
-var roundedDailyCost = dailyPassCost.toFixed(2);
 
-console.log(busPassCostNum);
 
-console.log("Your bus pass costs £" + busPassCostNum);
-console.log("If you got bus passes every day instead it would cost £" + (weeksForPass * daysPerWeek * daySaver));
-console.log("The bus pass price per day is £" + roundedDailyCost);
-console.log("That's £" + (daySaverNum - roundedDailyCost).toFixed(2) + " less per day than a day saver that costs £" + daySaverNum);}
+
+if (busPassCost.charAt(0) == "£"){busPassCost = busPassCost.slice(1);}
+busPassCost = Number(busPassCost);
+busPassCost = busPassCost.toFixed(2);
+if (daySaver.charAt(0) == "£"){daySaver = daySaver.slice(1);}
+daySaver = Number(daySaver);
+daySaver = daySaver.toFixed(2);
+var dailyPassCost = (busPassCost / (weeksForPass * daysPerWeek)).toFixed(2);
+var dailySaving = (daySaver - dailyPassCost).toFixed(2);
+var totalDaySavers = (weeksForPass * daysPerWeek * daySaver).toFixed(2);
+
+/*console.log(busPassCost);
+console.log(daySaver);
+console.log(dailyPassCost);
+console.log(dailySaving);*/
+
+
+console.log("Your bus pass costs £" + busPassCost);
+console.log("If you got bus passes every day instead it would cost £" + totalDaySavers);
+console.log("The bus pass price per day is £" + dailyPassCost);
+if (dailySaving > 0) {console.log("That's £" + dailySaving + " less per day than a day saver that costs £" + daySaver);}
+if (dailySaving < 0) {console.log("Umm, that's not a saving. Maybe just buy day savers.");} }
 
 busPassSavings();
